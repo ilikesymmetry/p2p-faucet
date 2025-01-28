23ac159f45731648d41cb9f8543c02546645695b
Conner Swenberg <conner.swenberg@coinbase.com>
2025-01-28 13:47:39 -0800

    Add fauct out of funds warning

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { useAccount, useBalance, useConnect, useDisconnect } from "wagmi";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { baseSepolia } from "viem/chains";
+import { parseUnits } from "viem";
 
 export default function App() {
   const account = useAccount()
@@ -31,10 +33,16 @@ export default function App() {
     }
   }
 
+  const {data, isLoading} = useBalance({address: "0x2B654aB28f82a2a4E4F6DB8e20791E5AcF4125c6", chainId: baseSepolia.id})
+  
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <button onClick={handleConnect} disabled={disabled} className="bg-[#0052FF] px-12 py-3 rounded-md font-bold disabled:opacity-60">Connect</button>
-      <div className="opacity-80 text-center mt-4">You will be redirect after connection. <br /> Refresh the redirected-to page after if the transaction does not show.</div>
+      {!isLoading && data!.value < parseUnits("0.002", 18) ? (
+        <div className="opacity-80 text-center mt-4">Faucet needs more funds.</div>
+      ) : (
+        <div className="opacity-80 text-center mt-4">You will be redirect after connection. <br /> Refresh the redirected-to page after if the transaction does not show.</div>
+      )}
     </div>
   );
 }
